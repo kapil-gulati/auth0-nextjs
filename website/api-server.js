99c9ea2cf5a3a1f1e8fac1575dfc6b359104f9e1
Kapil Gulati <kapil.gulati@abbott.com>
2023-07-07 23:27:06 -0500

    Minor updates to working example

--- website/api-server.js
+++ website/api-server.js
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: './.env.local' });
+require('dotenv').config({path: './.env.local'});
 
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const { expressjwt: jwt } = require('express-jwt');
+const {expressjwt: jwt} = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
 const app = express();
@@ -14,34 +14,44 @@ const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
 const audience = process.env.AUTH0_AUDIENCE;
 
 if (!baseUrl || !issuerBaseUrl) {
-  throw new Error('Please make sure that the file .env.local is in place and populated');
+    throw new Error('Please make sure that the file .env.local is in place and populated');
 }
 
 if (!audience) {
-  console.log('AUTH0_AUDIENCE not set in .env.local. Shutting down API server.');
-  process.exit(1);
+    console.log('AUTH0_AUDIENCE not set in .env.local. Shutting down API server.');
+    process.exit(1);
 }
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors({ origin: baseUrl }));
+app.use(cors({origin: baseUrl}));
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`
-  }),
-  audience: audience,
-  issuer: `${issuerBaseUrl}/`,
-  algorithms: ['RS256']
-});
+const checkJwt = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    console.log(bearerHeader);
+    if (bearerHeader) {
+        jwt({
+            secret: jwksRsa.expressJwtSecret({
+                cache: true,
+                rateLimit: true,
+                jwksRequestsPerMinute: 5,
+                jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`
+            }),
+            audience: audience,
+            issuer: `${issuerBaseUrl}/`,
+            algorithms: ['RS256']
+        });
+
+        next();
+    }else{
+        res.sendStatus(401);
+    }
+}
 
 app.get('/api/shows', checkJwt, (req, res) => {
-  res.send({
-    msg: 'Your access token was successfully validated!'
-  });
+    res.send({
+        msg: 'Your access token was successfully validated!'
+    });
 });
 
 const server = app.listen(port, () => console.log(`API Server listening on port ${port}`));
--- website/pages/api/shows.js
+++ website/pages/api/shows.js
@@ -1,4 +1,32 @@
+import ShortUniqueId from 'short-unique-id'
+import { v4 as uuidv4 } from 'uuid'
 import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+
+export function generateCustomerNumber(locale, env) {
+  let customerNumber
+  const uid = new ShortUniqueId({ dictionary: 'alphanum_upper' })
+  if (locale === 'en-GB') {
+    if (env === 'dev') {
+      customerNumber = uid.randomUUID(8)
+      customerNumber = `91${customerNumber}`
+      return customerNumber
+    }
+    if (env === 'qa') {
+      customerNumber = uid.randomUUID(8)
+      customerNumber = `92${customerNumber}`
+      return customerNumber
+    }
+    if (env === 'stage') {
+      customerNumber = uid.randomUUID(8)
+      customerNumber = `93${customerNumber}`
+      return customerNumber
+    }
+    const firstDigit = Math.floor(Math.random() * 9)
+    customerNumber = uid.randomUUID(8)
+    customerNumber = `${firstDigit}${customerNumber}`
+  }
+  return customerNumber
+}
 
 export default withApiAuthRequired(async function shows(req, res) {
   try {
@@ -12,6 +40,17 @@ export default withApiAuthRequired(async function shows(req, res) {
       }
     });
     const shows = await response.json();
+    const userId = uuidv4()
+
+    console.log(generateCustomerNumber('en-GB', 'prod'))
+    console.log(userId)
+
+    // const response4 = await fetch(`http://localhost:4000/api/private`, {
+    //   headers: {
+    //     Authorization: `Bearer ${accessToken}`
+    //   }
+    // });
+    //console.log(response4)
 
     res.status(200).json(shows);
   } catch (error) {
